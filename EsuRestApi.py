d4213c50a92f9548c2f4c9521d16a0b00a3ddfef
rajbala <rajbala@gmail.com>
2011-01-22 23:07:52 +0000

    Added the set_user_metadata() method that takes an object_id and optionally either listable and/or non-listable metadata in the form of a Python dictionary.

--- EsuRestApi.py
+++ EsuRestApi.py
@@ -369,7 +369,48 @@ class EsuRestApi(object):
          
         else:                                                                                               # If there was no HTTPError, parse the location header in the response body to get the object_id
             return response
+
+    def set_user_metadata(self, object_id, listable_meta = None, non_listable_meta = None):
+            
+        mime_type = "application/octet-stream" 
+        now = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
+    
+        headers = "POST\n"
+        headers += mime_type+"\n"
+        headers += "\n"
+        headers += now+"\n"
+        headers += "/rest/objects/"+object_id+"?metadata/user"+"\n"
+        headers += "x-emc-date:"+now+"\n"
+     
+        #request = urllib2.Request(self.url+"/rest/objects/"+object_id+"?metadata/user")
+        request = RequestWithMethod("POST", "%s/%s" % (self.url+"/rest/objects", object_id+"?metadata/user"))
+        request.add_header("content-type", mime_type)
+        request.add_header("date", now)
+        request.add_header("host", self.host)
+        request.add_header("x-emc-date", now)
         
+        if listable_meta:
+            meta_string = self.__process_metadata(listable_meta)
+            headers += "x-emc-listable-meta:"+meta_string+"\n"
+            request.add_header("x-emc-listable-meta", meta_string)
+            
+        if non_listable_meta:
+            nl_meta_string = self.__process_metadata(non_listable_meta)
+            headers += "x-emc-meta:"+nl_meta_string+"\n"
+            request.add_header("x-emc-meta", nl_meta_string)
+     
+        headers += "x-emc-uid:"+self.uid
+        request.add_header("x-emc-uid", self.uid)
+    
+        hashout = self.__sign(headers)
+     
+        try:
+            response = self.__send_request(request, hashout, headers)
+      
+        except urllib2.HTTPError as e:
+            error_message = e.read()
+            print error_message
+            
     def delete_user_metadata(self, object_id, metadata_key):
       
         mime_type = "application/octet-stream"
@@ -603,15 +644,6 @@ class RequestWithMethod(urllib2.Request):                                       
 #       We're also not doing range updates so large objects will be a problem at the moment.
    
     
-
-    
-
-    
-    def get_listable_tags():
-        pass
-    
-    def list_user_metadata():
-        pass
     
     def set_user_metadata():
         pass
