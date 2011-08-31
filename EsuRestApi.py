4d33bee0735e6bdc244541eef8f70f3e360f9568
rajbala <rajbala@gmail.com>
2011-08-31 14:32:16 +0000

    Fixed bug where get_shareable_url wouldn't work properly with object_ids only.

--- EsuRestApi.py
+++ EsuRestApi.py
@@ -589,7 +589,7 @@ class EsuRestApi(object):
         
         """
         
-        if path[0] == "/":
+        if path and path[0] == "/":
             path = path[1:]
         
         if object_id and path:
